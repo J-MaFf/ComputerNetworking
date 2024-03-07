9fb0495fcb62e7c143caaa746a2d4d07ab223b84
Joey Maffiola <7maffiolajoey@gmail.com>
2024-03-07 12:42:18 -0600

    Socket connection + hello draft complete.

---
+++ server.py
@@ -0,0 +1,25 @@
+import socket
+
+def main():
+    HOST = "127.0.0.1" # The server's hostname or IP address
+    PORT = 7777 # The port used by the server
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        #                     (^ IPV4     ,      ^ TCP)
+        s.bind(HOST, PORT)
+        s.listen()
+        conn, addr = s.accept() 
+        ''' con = new socket object usable to send and receive data
+            on the connection, addr = address bound to the socket on
+            the other end of the connection
+        '''
+        with conn:
+            print(f"Connected to {addr}")
+            while True:
+                data = conn.recv(1024) 
+                if not data:
+                    break
+                conn.sendall(data)
+
+if __name__ == "__main__": # Only run the code if this file is run directly
+    main()
